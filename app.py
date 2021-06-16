92a6bae23f4862be9e78fcbe0d1cea3e9dab9f89
sharmashivam27 <shivam.94635@gmail.com>
2021-06-16 21:51:19 +0530

    All set

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask,redirect,url_for,render_template
 from flask.helpers import send_file
+from flask_restful import Api,Resource
+
+file={0:r"C:\Users\Sharma\Desktop\Flask\Files\Ultrasonic.ino"}
 
 app=Flask(__name__)
+api=Api(app)
+
+class files(Resource):
+    def get(self,file_id):
+        if file_id in file:
+            return send_file(file[file_id],as_attachment=True)
+        else:
+            return  'File not Found'  
+
+api.add_resource(files,"/download/<int:file_id>")
+
 @app.route("/home")
 @app.route("/")
 def home():
@@ -11,9 +25,7 @@ def home():
 def content_():
     return render_template("content.html")
 
-@app.route("/download")
-def download():
-    p=r"C:\Users\Sharma\Desktop\Flask\Files\Ultrasonic.ino"
-    return send_file(p,as_attachment=True)
+
+
 if __name__=="__main__":
     app.run(debug=True)
